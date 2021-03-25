a7854f3afc515aec5221a0f2ad63f8b8944a36a1
Sean <thesean.kim33@gmail.com>
2021-03-25 03:43:27 -0500

    #29 - Modals

--- screens/home.js
+++ screens/home.js
@@ -1,13 +1,15 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList } from 'react-native';
+import { View, Text, FlatList, Modal, StyleSheet, Button } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 import { globalStyles } from '../styles/global';
 import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
 import Card from '../shared/card'
 
 const Home = () => {
 
     const navigation = useNavigation();
+    const[modalOpen, setModalOpen] = useState(false);
 
     const [reviews, setReviews] = useState([
         { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
@@ -15,12 +17,29 @@ const Home = () => {
         { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
     ]);
 
-    const pressHandler = () => {
-        navigation.navigate('Details')
-    }
     
     return (
         <View style={globalStyles.container}>
+
+            <Modal visible={modalOpen} animationType='slide'>
+                <View style={styles.modalContent}>
+                    <MaterialIcons
+                        name='close'
+                        size={24}
+                        style={{...styles.modalToggle, ...styles.modalClose}}
+                        onPress={() => setModalOpen(false)}
+                    />
+                    <Text>Hello from the Modal</Text>
+                </View>
+            </Modal>
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
+            />
+
             <FlatList
                 data={reviews}
                 renderItem={({ item }) => (
@@ -34,5 +53,23 @@ const Home = () => {
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    modalContent: {
+        flex: 1,
+    },
+    modalToggle: {
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center'
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    }
+})
 
 export default Home
